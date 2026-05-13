8746509474608c367c6cf4bd81c3f270248821f4
JuniorVln <junior@volna.today>
2026-05-12 21:24:14 -0300

    feat: enrich leads with verified LinkedIn profiles and business emails

--- Prospects/decisores-veterinarios-enriched.js
+++ Prospects/decisores-veterinarios-enriched.js
@@ -1,117 +1,186 @@
 window.LEAD_ENRICHMENT = [
   {
     key: "fully matched|1|elaine santos|noxon saude animal industria farmaceutica veterinaria",
-    linkedinUrl: "https://br.linkedin.com/in/elaine-santos-9a843a46",
-    description: "Elaine Santos. NOXON SAÚDE ANIMAL - Indústria Farmacêutica Veterinária"
+    linkedinUrl: "https://www.linkedin.com/in/elaine-santos-9a843a46",
+    emails: ["elaine.santos@noxon.com.br"],
+    websiteUrl: "http://www.noxon.com.br",
+    description: "Compradora · NOXON SAÚDE ANIMAL - Indústria Farmacêutica Veterinária",
+    enrichmentStatus: "done"
   },
   {
     key: "fully matched|2|ezequias neto|mundo animal laboratorio veterinario",
-    linkedinUrl: "https://br.linkedin.com/pub/dir/Ezequias/Neto/br-0-Brasil",
-    description: "proprietário na Mundo Animal Laboratório Veterinário"
+    linkedinUrl: "https://www.linkedin.com/in/ezequias-espindola-neto-95117956",
+    emails: [],
+    websiteUrl: "",
+    description: "Dir. proprietário · Mundo Animal Laboratório Veterinário · Goiânia, GO",
+    enrichmentStatus: "done"
   },
   {
     key: "fully matched|3|luiz marcelino|agrofort",
     linkedinUrl: "https://br.linkedin.com/pub/dir/Luiz/Marcelino/br-0-Brasil",
-    description: "Sócio - Proprietário AGROFORT"
+    emails: [],
+    websiteUrl: "",
+    description: "Sócio - Proprietário · AGROFORT",
+    enrichmentStatus: "done"
   },
   {
     key: "fully matched|4|eliezer barros|vetbr saude animal",
-    linkedinUrl: "https://br.linkedin.com/in/eliezer-barros",
-    description: "VetBR Saúde Animal"
+    linkedinUrl: "https://www.linkedin.com/in/eliezer-barros",
+    emails: ["eliezer.barros@vetbr.com.br"],
+    websiteUrl: "http://www.vetbr.com.br",
+    description: "COO - Diretor de Operações e RH · VetBR Saúde Animal",
+    enrichmentStatus: "done"
   },
   {
     key: "fully matched|5|caio salomao|vetquest",
-    linkedinUrl: "https://br.linkedin.com/in/caio-salom%C3%A3o-7ab80395",
-    description: "Gerente Geral na VetQuest"
+    linkedinUrl: "https://www.linkedin.com/in/caio-salom%C3%A3o-7ab80395",
+    emails: ["caio.salomao@vetquest.com.br"],
+    websiteUrl: "https://vetquest.com.br",
+    description: "Gerente Geral · VetQuest · Campinas, SP",
+    enrichmentStatus: "done"
   },
   {
     key: "fully matched|6|claudia silva|distribuidora playpet domus",
-    linkedinUrl: "https://br.linkedin.com/in/claudia-silva-129290384",
-    description: "Direto gerente Gerente Administrativo na Distribuidora Playpet Domus"
+    linkedinUrl: "https://www.linkedin.com/in/claudia-silva-129290384",
+    emails: [],
+    websiteUrl: "",
+    description: "Diretora Administrativa · Distribuidora Playpet Domus · São Paulo, SP",
+    enrichmentStatus: "done"
   },
   {
     key: "fully matched|7|paola turazza|cooper nutri",
-    linkedinUrl: "https://br.linkedin.com/in/paola-turazza-83341a147",
-    description: "Diretora na Cooper Nutri Rações e Produtos Agropecuários"
+    linkedinUrl: "https://www.linkedin.com/in/paola-turazza-83341a147",
+    emails: [],
+    websiteUrl: "http://coopernutri.com.br",
+    description: "Diretora · Cooper Nutri Rações e Produtos Agropecuários · São Paulo, SP",
+    enrichmentStatus: "done"
   },
   {
     key: "fully matched|8|vitor franceschini|farmabase saude animal ltda",
-    linkedinUrl: "https://br.linkedin.com/in/vitor-franceschini-b0852510",
-    description: "Executive MBA | Fundação Dom Cabral. Farmabase"
+    linkedinUrl: "https://www.linkedin.com/in/vitor-franceschini-b0852510",
+    emails: ["vitor@farmabase.com"],
+    websiteUrl: "http://www.farmabase.com",
+    description: "Diretor Executivo - CEO · Farmabase Saúde Animal · Jaguariúna, SP",
+    enrichmentStatus: "done"
   },
   {
     key: "fully matched|9|heitor caetano|ja saude animal",
-    linkedinUrl: "https://br.linkedin.com/in/heitorcaetano",
-    description: "JA Saúde Animal"
+    linkedinUrl: "https://www.linkedin.com/in/heitorcaetano",
+    emails: ["heitor.caetano@jasaudeanimal.com.br"],
+    websiteUrl: "http://www.jasaudeanimal.com.br",
+    description: "CFO · JA Saúde Animal · Patrocínio Paulista, SP",
+    enrichmentStatus: "done"
   },
   {
     key: "fully matched|10|thiago goulart|vetquest",
-    linkedinUrl: "https://br.linkedin.com/in/thiago-goulart-41500972",
-    description: "Formado em Medicina Veterinária pela Universidade Federal do Paraná"
+    linkedinUrl: "https://www.linkedin.com/in/thiago-goulart-41500972",
+    emails: ["thiago.goulart@vetquest.com.br"],
+    websiteUrl: "https://vetquest.com.br",
+    description: "Sócio Proprietário · VetQuest · Toledo, PR",
+    enrichmentStatus: "done"
   },
   {
     key: "fully matched|11|aline martins|calbos saude animal",
-    linkedinUrl: "https://br.linkedin.com/in/aline-cristina-martins",
-    description: "Diretora Administrativa Financeira na Calbos Saúde Animal"
+    linkedinUrl: "https://www.linkedin.com/in/aline-cristina-martins",
+    emails: [],
+    websiteUrl: "http://www.calbos.com.br",
+    description: "Diretora Administrativa Financeira · Calbos Saúde Animal · Curitiba, PR",
+    enrichmentStatus: "done"
   },
   {
     key: "fully matched|12|leonardo amoedo|amoedo distribuidora",
-    linkedinUrl: "https://br.linkedin.com/in/leonardo-amoedo-2950a6133",
-    description: "CEO Grupo Amoedo"
+    linkedinUrl: "https://www.linkedin.com/in/leonardo-amoedo-2950a6133",
+    emails: [],
+    websiteUrl: "",
+    description: "CEO Grupo Amoedo · Salvador, BA",
+    enrichmentStatus: "done"
   },
   {
     key: "fully matched|13|sergio marchetti|ja saude animal",
-    linkedinUrl: "https://br.linkedin.com/pub/dir/S%C3%A9rgio/Marchetti/br-0-Brasil",
-    description: "JA Saúde Animal"
+    linkedinUrl: "https://www.linkedin.com/in/s%C3%A9rgio-ricardo-marchetti-98603932",
+    emails: [],
+    websiteUrl: "http://www.jasaudeanimal.com.br",
+    description: "Gerente Administrativo e Financeiro · JA Saúde Animal",
+    enrichmentStatus: "done"
   },
   {
     key: "fully matched|14|mauricio malheiros|ja saude animal",
-    linkedinUrl: "https://br.linkedin.com/in/mauricio-malheiros",
-    description: "JA Saúde Animal"
+    linkedinUrl: "https://www.linkedin.com/in/mauricio-malheiros",
+    emails: ["mauricio.malheiros@jasaudeanimal.com.br"],
+    websiteUrl: "http://www.jasaudeanimal.com.br",
+    description: "Supply Chain · JA Saúde Animal",
+    enrichmentStatus: "done"
   },
   {
     key: "fully matched|15|maira billi|pearson saude animal",
-    linkedinUrl: "https://br.linkedin.com/in/maira-billi-656699360",
-    description: "CEO na Pearson Saúde Animal"
+    linkedinUrl: "https://www.linkedin.com/in/maira-billi-656699360",
+    emails: [],
+    websiteUrl: "",
+    description: "CEO · Pearson Saúde Animal",
+    enrichmentStatus: "done"
   },
   {
     key: "fully matched|16|luiz luccas|ja saude animal",
-    linkedinUrl: "https://br.linkedin.com/in/luizluccas",
-    description: "JA Saúde Animal"
+    linkedinUrl: "https://www.linkedin.com/in/luizluccas",
+    emails: [],
+    websiteUrl: "http://www.jasaudeanimal.com.br",
+    description: "JA Saúde Animal",
+    enrichmentStatus: "done"
   },
   {
     key: "fully matched|17|nicole godinho|vet vale saude animal",
-    linkedinUrl: "https://br.linkedin.com/in/nicole-godinho-9a97b169",
-    description: "Vet Vale Saúde Animal"
+    linkedinUrl: "https://www.linkedin.com/in/nicole-godinho-9a97b169",
+    emails: [],
+    websiteUrl: "",
+    description: "Vet Vale Saúde Animal",
+    enrichmentStatus: "done"
   },
   {
     key: "fully matched|18|marcos sobrinho|distribuidora playpet domus",
-    linkedinUrl: "https://br.linkedin.com/in/marcos-joan%C3%ADcio-cerrini-de-calda-sobrinho-6a1b77207",
-    description: "CEO e Coofoundador da Distribuidora Playpet Domus"
+    linkedinUrl: "https://www.linkedin.com/in/marcos-joan%C3%ADcio-cerrini-de-calda-sobrinho-6a1b77207",
+    emails: [],
+    websiteUrl: "",
+    description: "CEO e Cofundador · Distribuidora Playpet Domus · São Paulo, SP",
+    enrichmentStatus: "done"
   },
   {
     key: "fully matched|19|fernando nogueira|veterinup saude animal",
-    linkedinUrl: "https://br.linkedin.com/in/fernando-braga-nogueira-12b82233a",
-    description: "CEO da VeterinUP Saúde Animal"
+    linkedinUrl: "https://www.linkedin.com/in/fernando-braga-nogueira-12b82233a",
+    emails: [],
+    websiteUrl: "",
+    description: "CEO · VeterinUP Saúde Animal",
+    enrichmentStatus: "done"
   },
   {
     key: "fully matched|20|rafael mio|vetquest",
-    linkedinUrl: "https://br.linkedin.com/in/rafael-theisen-mio-75288b144",
-    description: "Experiência: VetQuest"
+    linkedinUrl: "https://www.linkedin.com/in/rafael-theisen-mio-75288b144",
+    emails: [],
+    websiteUrl: "https://vetquest.com.br",
+    description: "VetQuest",
+    enrichmentStatus: "done"
   },
   {
     key: "partially matched|1|provet veterinaria|provet nordic",
-    linkedinUrl: "https://br.linkedin.com/company/provetdiag",
-    description: "Com mais de 700 exames disponíveis"
+    linkedinUrl: "https://www.linkedin.com/company/provetdiag",
+    emails: [],
+    websiteUrl: "",
+    description: "Provet Veterinária · Diagnósticos · +700 exames disponíveis",
+    enrichmentStatus: "done"
   },
   {
     key: "partially matched|2|roberto fecchio|clinica safari",
-    linkedinUrl: "https://br.linkedin.com/in/roberto-fecchio-793519238",
-    description: "Clínica Safari"
+    linkedinUrl: "https://www.linkedin.com/in/roberto-fecchio-793519238",
+    emails: [],
+    websiteUrl: "",
+    description: "Clínica Safari · Medicina Veterinária",
+    enrichmentStatus: "done"
   },
   {
     key: "partially matched|3|cassiano bertoletti|hospidez comercio de produtos medico hospitalares ltda",
-    linkedinUrl: "https://br.linkedin.com/pub/dir/Cassiano/Bertoletti/br-0-Brasil",
-    description: "Diretor Comercial (Sócio -"
+    linkedinUrl: "https://www.linkedin.com/in/cassiano-rafael-bertoletti-318833162",
+    emails: [],
+    websiteUrl: "",
+    description: "Diretor Comercial (Sócio-Proprietário) · HOSPIDEZ Comércio de Produtos Médico-Hospitalares",
+    enrichmentStatus: "done"
   }
 ];
